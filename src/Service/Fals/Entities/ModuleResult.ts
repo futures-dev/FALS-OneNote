5c717614338ea6b9ffc85527ab5b1c2fda69bbe9
Andrei <futures_dev@outlook.com>
2018-01-09 12:55:43 +0300

    ModuleResult : explicit Student field not needed due to Course field

--- src/Service/Fals/Entities/ModuleResult.ts
+++ src/Service/Fals/Entities/ModuleResult.ts
@@ -8,20 +8,17 @@ import { Module } from './Module';
 /**
  * @author Computer
  * @version 1.0
- * @created 24-дек-2017 20:28:47
+ * @created 24-пїЅпїЅпїЅ-2017 20:28:47
  * @class
  * @extends ModuleStatistics
  */
 export class ModuleResult extends ModuleStatistics {
-    public student : Student;
-
     public module : Module;
 
     public result : any;
 
     public constructor() {
         super();
-        this.student = null;
         this.module = null;
         this.result = null;
     }
