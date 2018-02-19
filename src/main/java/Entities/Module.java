6019e214377ffdccd613a02d9728a309f6096b4a
Andrei@Computer <futures_dev@outlook.com>
2018-02-19 11:58:31 +0300

    InterventionModel associations

--- src/main/java/Entities/Module.java
+++ src/main/java/Entities/Module.java
@@ -5,6 +5,7 @@ public class Module {
 
 	public Step steps[];
 	public int maxGrade;
+	public ModuleInterventionModel possibleInterventions[];
 
 	public Module(){
 
--- src/main/java/Entities/Step.java
+++ src/main/java/Entities/Step.java
@@ -5,6 +5,7 @@ public abstract class Step {
 
 	public String resultType;
 	public int maxGrade;
+	public StepInterventionModel possibleInterventions[];
 
 	public Step(){
 
