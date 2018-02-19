56c70b9c5b3affa3ff5e0a2a7b7e56e2e2778351
Andrei@Computer <futures_dev@outlook.com>
2018-02-19 10:27:31 +0300

    Lazy wrapper included in Java model

---
+++ src/Service/Fals/Entities/Lazy/CourseWrapper.ts
@@ -0,0 +1,17 @@
+/* Auto-generated file */
+
+import {Period, Type} from "Service/Fals/TypeMap"
+
+import { Course } from '../Course'; 
+import { ILazyWrapper } from './ILazyWrapper'; 
+export class CourseWrapper extends Course implements ILazyWrapper {
+    public constructor() {
+        super();
+    }
+}
+CourseWrapper["__class"] = "Entities.Lazy.CourseWrapper";
+CourseWrapper["__interfaces"] = ["Entities.Lazy.ILazyWrapper"];
+
+
+
+
---
+++ src/main/java/Entities/Lazy/CourseWrapper.java
@@ -0,0 +1,11 @@
+package Entities.Lazy;
+
+import Entities.Course;
+
+public class CourseWrapper extends Course implements ILazyWrapper {
+
+	public CourseWrapper(){
+
+	}
+
+}
