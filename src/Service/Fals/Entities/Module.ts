86d541f6be7c120803afba5adf231ccde6677ccb
Andrei@Computer <futures_dev@outlook.com>
2018-02-19 11:59:46 +0300

    ModuleIntervention associations

--- src/Service/Fals/Entities/Module.ts
+++ src/Service/Fals/Entities/Module.ts
@@ -2,14 +2,18 @@
 
 import { Period, Type } from "Service/Fals/TypeMap";
 import { Step } from "./Step";
+import { ModuleInterventionModel } from "./ModuleInterventionModel";
 export class Module {
   public steps: Step[];
 
   public maxGrade: number;
 
+  public possibleInterventions: ModuleInterventionModel[];
+
   public constructor() {
     this.steps = null;
     this.maxGrade = 0;
+    this.possibleInterventions = null;
   }
 }
 Module["__class"] = "Entities.Module";
--- src/Service/Fals/Entities/Step.ts
+++ src/Service/Fals/Entities/Step.ts
@@ -1,14 +1,18 @@
 /* Auto-generated file */
 
 import { Period, Type } from "Service/Fals/TypeMap";
+import { StepInterventionModel } from "./StepInterventionModel";
 export abstract class Step {
   public resultType: string;
 
   public maxGrade: number;
 
+  public possibleInterventions: StepInterventionModel[];
+
   public constructor() {
     this.resultType = null;
     this.maxGrade = 0;
+    this.possibleInterventions = null;
   }
 }
 Step["__class"] = "Entities.Step";
