143d13a65304dfc2084fdc1e44a5ff142f4ec1d6
futures-dev <futures_dev@outlook.com>
2018-04-20 20:53:54 +0300

    Welcome.html crutch

--- src/View/Main/WelcomeComponent.ts
+++ src/View/Main/WelcomeComponent.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit, EventEmitter } from "@angular/core";
 import { OneNoteAuth } from "Service/Office/Auth/OneNoteAuth";
 import { InitializationPublisher } from "Service/Office/InitializationPublisher";
+import { BehaviorSubject } from "rxjs";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "welcome",
   templateUrl: "View/Main/Welcome.html",
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements AfterViewInit {
   constructor(
     private onenote: OneNoteAuth,
     private initializationPublisher: InitializationPublisher
-  ) {}
+  ) {
+    console.log("welcome ctor");
+  }
 
-  enabled = this.onenote.isAuth;
+  public isAuth = new EventEmitter<boolean>(true);
+
+  qqq = new EventEmitter<string>();
 
   login() {
     this.initializationPublisher.executeAfterInit(() =>
@@ -20,5 +26,39 @@ export class WelcomeComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {}
+  t1 = `<header class="ms-welcome__header ms-bgColor-neutralLighter ms-u-fadeIn500">
+  <img width="90" height="90" src="assets/logo-filled.png" alt="FALS-OneNote" title="FALS-OneNote" />  
+  <div>
+      <button (click)="login()">Войти</button>
+  </div>
+</header>`;
+  t2 = `<header class="ms-welcome__header ms-bgColor-neutralLighter ms-u-fadeIn500">
+  <img width="90" height="90" src="assets/logo-filled.png" alt="FALS-OneNote" title="FALS-OneNote" />
+  <h1 class="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">Welcome</h1>
+  </header>`;
+
+  // GODDAMN CRUTCH
+
+  ngAfterViewInit(): void {
+    this.qqq.emit(this.t2);
+    this.onenote.isAuth.subscribe(q => {
+      console.log("Welcome: auth = " + q);
+      //this.qqq.emit(q ? this.t2 : this.t1);
+      this.queue.push(q);
+      window.setTimeout(() => this.popQueue(), 1000);
+    });
+  }
+
+  private popQueue() {
+    const b = this.queue.shift();
+    console.log("settimeout " + b);
+    if (b !== undefined) {
+      this.qqq.emit(b ? this.t2 : this.t1);
+      if (this.queue.length > 0) {
+        window.setTimeout(() => this.popQueue(), 1000);
+      }
+    }
+  }
+
+  private readonly queue = [];
 }
