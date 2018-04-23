7f5877e211bcad9b5530f35932cfc7233722937e
futures-dev <futures_dev@outlook.com>
2018-04-23 10:51:35 +0300

    CLIENT_URL in cfg.js

--- config/heroku/cfg.js
+++ config/heroku/cfg.js
@@ -1,4 +1,5 @@
 module.exports = {
   SOCKET_URL: "https://hidden-bastion.herokuapp.com",
   SERVER_URL: "https://hidden-bastion.herokuapp.com",
+  CLIENT_URL: "https://hidden-bastion.herokuapp.com"
 };
