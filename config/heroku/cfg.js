bcc89f5f5092251fdeef0a991e802a8cb4a66c31
futures-dev <futures_dev@outlook.com>
2018-06-03 19:48:09 +0300

    manifest fix

--- config/heroku/cfg.js
+++ config/heroku/cfg.js
@@ -1,5 +1,9 @@
 module.exports = {
   SOCKET_URL: "https://fals-on.herokuapp.com",
   SERVER_URL: "https://fals-on.herokuapp.com",
-  CLIENT_URL: "https://fals-on.herokuapp.com"
+  CLIENT_URL: "https://fals-on.herokuapp.com",
+  GUID: "fals-guid",
+  EMAIL: "fals-email",
+  DISPLAY_NAME: "fals-display-name",
+  NO_OFFICE: false
 };
