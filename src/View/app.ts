1ed96dca8cfaa2c0847599be1d7cfc7be431097f
futures-dev <futures_dev@outlook.com>
2018-06-03 20:25:01 +0300

    try fix

--- src/View/app.ts
+++ src/View/app.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { MainModule } from "View/Main/Module";
+import { enableProdMode } from "@angular/core";
 
 // when Office has initalized, manually bootstrap the app
 (() => {
+  enableProdMode();
   platformBrowserDynamic().bootstrapModule(MainModule);
 })();
